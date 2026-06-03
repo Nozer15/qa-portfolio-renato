1c6a6010097104d45447f5eb7fbec5b1a3430b87
Renato Freitas <freitas.renatoc@hotmail.com>
2026-06-03 14:34:04 -0300

    fix: corrigir sintaxe e melhorar sincronização nos testes ET00, ET01 e ET04

--- saucedemo-test/03-automation/cypress/e2e/et00-problem-user.cy.js
+++ saucedemo-test/03-automation/cypress/e2e/et00-problem-user.cy.js
@@ -4,9 +4,12 @@ describe('ET00 - Login com usuário problemático', () => {
     cy.get('[data-test="username"]').type('problem_user')
     cy.get('[data-test="password"]').type('secret_sauce')
     cy.get('[data-test="login-button"]').click()
+    cy.get('.inventory_list').should('be.visible')
   })
 
   it('deve exibir imagens diferentes em cada produto (bug esperado)', () => {
+    cy.get('.inventory_item_img img').should('have.length.greaterThan', 0)
+    
     const imagens = []
     cy.get('.inventory_item_img img').each(($img) => {
       imagens.push($img.attr('src'))
--- saucedemo-test/03-automation/cypress/e2e/et01-adicionar-todos-refresh.cy.js
+++ saucedemo-test/03-automation/cypress/e2e/et01-adicionar-todos-refresh.cy.js
@@ -2,15 +2,23 @@ describe('ET01 - Adicionar todos os produtos e atualizar a página', () => {
   beforeEach(() => {
     cy.visit('/')
     cy.login()
+    cy.get('.inventory_list').should('be.visible')
   })
 
   it('deve manter todos os produtos no carrinho após refresh', () => {
-    cy.get('[class*="add-to-cart"]').each(($btn) => {
+    // Adicionar todos os produtos
+    cy.get('[class*="add-to-cart"]').each(($btn, index) => {
       cy.wrap($btn).click()
+      cy.get('.shopping_cart_badge').should('have.text', String(index + 1))
     })
-    cy.get('.shopping_cart_badge').then(($badge) => {
-      const total = $badge.text()
-      cy.reload()
+
+    // Contar quantos produtos foram adicionados
+    cy.get('.shopping_cart_badge').should('exist').invoke('text').as('cartTotal')
+
+    // Reload e validar
+    cy.reload()
+    cy.get('.inventory_list').should('be.visible')
+    cy.get('@cartTotal').then(total => {
       cy.get('.shopping_cart_badge').should('have.text', total)
     })
   })
